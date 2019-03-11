9e522d14398cb74d1f42c93266bce5fe46dd0697
Ewan <fanyizeng@yeah.net>
2019-03-11 19:32:59 +0800

    log

--- Tests/Foundation/EOS/EOSTransactionSignerTests.swift
+++ Tests/Foundation/EOS/EOSTransactionSignerTests.swift
@@ -13,22 +13,22 @@ import XCTest
 import CoreBitcoin
 
 class EOSTransactionSignerTests: TestCase {
-//  func testSignTransacton() {
-//    let wallet = try! WalletManager.importEOS(from: TestData.mnemonic, accountName: "", permissions: [], metadata: WalletMeta(chain: .eos, source: .mnemonic), encryptBy: TestData.password, at: BIP44.eos)
-//    let txs = [
-//      EOSTransaction(
-//        data: "c578065b93aec6a7c811000000000100a6823403ea3055000000572d3ccdcd01000000602a48b37400000000a8ed323225000000602a48b374208410425c95b1ca80969800000000000453595300000000046d656d6f00",
-//        publicKeys: ["EOS5SxZMjhKiXsmjxac8HBx56wWdZV1sCLZESh3ys1rzbMn4FUumU"],
-//        chainID: TestData.eosChainID
-//      )
-//    ]
-//    let result = try! EOSTransactionSigner(txs: txs, keystore: wallet.keystore, password: TestData.password).sign()
-//    XCTAssertEqual(1, result.count)
-//    XCTAssertEqual(
-//      result[0],
-//      EOSSignResult(hash: "6af5b3ae9871c25e2de195168ed7423f455a68330955701e327f02276bb34088", signs: ["SIG_K1_KkCTdqnTztAPnYeB2TWhrqcDhnnLvFJJdXnFCE3g8jRyz2heCggDQt5bMABu4LawHaDy4taHwJR3XMKV2ZXnBWqyiBnQ9J"])
-//    )
-//  }
+  func testSignTransacton() {
+    let wallet = try! WalletManager.importEOS(from: TestData.mnemonic, accountName: "", permissions: [], metadata: WalletMeta(chain: .eos, source: .mnemonic), encryptBy: TestData.password, at: BIP44.eos)
+    let txs = [
+      EOSTransaction(
+        data: "c578065b93aec6a7c811000000000100a6823403ea3055000000572d3ccdcd01000000602a48b37400000000a8ed323225000000602a48b374208410425c95b1ca80969800000000000453595300000000046d656d6f00",
+        publicKeys: ["EOS5SxZMjhKiXsmjxac8HBx56wWdZV1sCLZESh3ys1rzbMn4FUumU"],
+        chainID: TestData.eosChainID
+      )
+    ]
+    let result = try! EOSTransactionSigner(txs: txs, keystore: wallet.keystore, password: TestData.password).sign()
+    XCTAssertEqual(1, result.count)
+    XCTAssertEqual(
+      result[0],
+      EOSSignResult(hash: "6af5b3ae9871c25e2de195168ed7423f455a68330955701e327f02276bb34088", signs: ["SIG_K1_KkCTdqnTztAPnYeB2TWhrqcDhnnLvFJJdXnFCE3g8jRyz2heCggDQt5bMABu4LawHaDy4taHwJR3XMKV2ZXnBWqyiBnQ9J"])
+    )
+  }
 
   func testSignWithWrongPassword() {
     let wallet = try! WalletManager.importEOS(from: TestData.mnemonic, accountName: "", permissions: [], metadata: WalletMeta(chain: .eos, source: .mnemonic), encryptBy: TestData.password, at: BIP44.eos)
